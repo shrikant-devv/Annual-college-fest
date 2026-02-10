eb9e178874418918bee163b518e9571110d112e0
shrikant-devv <shrikantchinchole11@gmail.com>
2026-02-10 10:16:16 +0530

    Use environment PORT for Render deployment

--- server.js
+++ server.js
@@ -2,32 +2,32 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const DIR = __dirname;
 
 const server = http.createServer((req, res) => {
     let filePath = path.join(DIR, req.url === '/' ? '1st.html' : req.url);
-    
+
     fs.readFile(filePath, (err, data) => {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
             res.end('<h1>404 - File Not Found</h1>');
             return;
         }
-        
+
         const ext = path.extname(filePath);
         let contentType = 'text/html';
-        
+
         if (ext === '.css') contentType = 'text/css';
         else if (ext === '.js') contentType = 'application/javascript';
         else if (ext === '.png') contentType = 'image/png';
         else if (ext === '.jpg' || ext === '.jpeg') contentType = 'image/jpeg';
-        
+
         res.writeHead(200, { 'Content-Type': contentType });
         res.end(data);
     });
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
